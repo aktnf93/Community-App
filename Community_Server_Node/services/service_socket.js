e3556c4fdf8de6c0db386772a3d91b0368556331
B <B>
2025-12-29 09:57:06 +0900

    feat: server update (socket.io 시 chat 관련 db 를 사용하도록 수정)

--- Community_Server_Node/services/service_socket.js
+++ Community_Server_Node/services/service_socket.js
@@ -28,35 +28,16 @@ const initSocket = (server) => {
             console.log(`joinRoom > ${JSON.stringify(join)}`);
 
             try {
-                // signup
-                let qry = "INSERT IGNORE INTO tb_chat_members (chat_room_id, employee_id) VALUES (?, ?);";
-                const result = await db.query(req, qry, [ join.roomId, join.userId ]);
-                console.log(`joinRoom: 채팅방 멤버를 추가합니다. ${JSON.stringify(result)}`);
+                // 채팅방 멤버 추가
+                const result = await db.query(req, 'INSERT IGNORE INTO tb_chat_members (chat_room_id, employee_id) VALUES (?, ?);', [ join.roomId, join.userId ]);
 
-                // get members
-                qry = "SELECT * FROM tb_chat_members m WHERE m.chat_room_id = ?;";
-                const members = await db.query(req, qry, [ join.roomId ]);
-                console.log(`joinRoom: 채팅방 멤버를 불러옵니다. ${members}`);
+                // 채팅방 멤버 조회
+                const members = await db.query(req, 'SELECT * FROM tb_chat_members m WHERE m.chat_room_id = ?;', [ join.roomId ]);
 
-                // get messages
-                qry = `
-SELECT 
-	m.id AS 'Id',
-	m.chat_room_id AS 'Chat_Room_Id', 
-	m.employee_id AS 'Employee_Id',
-	e.name AS 'Employee_Name',
-	m.message AS 'Message',
-	DATE_FORMAT(m.created_at, '%Y-%m-%dT%H:%i:%s') AS 'Created_At',
-	DATE_FORMAT(m.deleted_at, '%Y-%m-%dT%H:%i:%s') AS 'Deleted_At'
-FROM tb_chat_messages m 
-	INNER JOIN tb_employees e ON m.employee_id = e.id
-WHERE m.chat_room_id = ?;
-                `;
-                const messages = await db.query(req, qry, [ join.roomId ]);
-                console.log(`joinRoom: 채팅방 메시지를 불러옵니다. ${messages}`);
+                // 채팅방 메시지 조회
+                const messages = await db.query(req, 'SELECT * FROM v_chat_messages m WHERE m.chat_room_id = ?;', [ join.roomId ]);
 
-                socket.join(join.roomId);
-                console.log(`joinRoom: 채팅방에 연결합니다. ${join.roomId}`);
+                socket.join(join.roomId); // 채팅방 연결.
 
                 io.to(join.roomId).emit('welcome', { 
                     Id: 0,
@@ -70,7 +51,6 @@ WHERE m.chat_room_id = ?;
                     Members: members, 
                     Messages: messages 
                 });
-                console.log(`joinRoom: 채팅방 멤버와 메시지를 응답합니다.`);
             }
             catch (err) {
                 console.log(err);
@@ -79,35 +59,14 @@ WHERE m.chat_room_id = ?;
         });
 
         socket.on('sendMessage', async (msg) => {
-            console.log('sendMessage');
-            console.log(msg);
+            console.log(`sendMessage > ${JSON.stringify(msg)}`);
 
             // DB 저장
-            const qry = "INSERT INTO tb_chat_messages (chat_room_id, employee_id, message) VALUES (?, ?, ?);";
-            const result = await db.query(req, qry, [ msg.Chat_Room_Id, msg.Employee_Id, msg.Message ]);
-            console.log(result);
-            const db_msg = await db.query(req, 
-`
-SELECT 
-	m.id AS 'Id',
-	m.chat_room_id AS 'Chat_Room_Id',
-	m.employee_id AS 'Employee_Id',
-	m.message AS 'Message',
-	DATE_FORMAT(m.created_at, '%Y-%m-%dT%H:%i:%s') AS 'Created_At',
-	DATE_FORMAT(m.deleted_at, '%Y-%m-%dT%H:%i:%s') AS 'Deleted_At', 
-	e.name AS 'Employee_Name' 
-FROM tb_chat_messages m 
-INNER JOIN tb_employees e ON m.employee_id = e.id 
-WHERE m.id = ?;
-`, 
-                [ result.insertId ]);
-
-            // msg.Created_At = db_msg.created_at;
-            // msg.Employee_Name = db_msg.Employee_Name;
+            const result = await db.query(req, 'INSERT INTO tb_chat_messages (chat_room_id, employee_id, message) VALUES (?, ?, ?);', [ msg.Chat_Room_Id, msg.Employee_Id, msg.Message ]);
+            const db_msg = await db.query(req, 'SELECT * FROM v_chat_messages m WHERE m.id = ?;', [result.insertId]);
 
             if (result) {
                 // 같은 방 사용자에게 브로드캐스트
-                console.log(db_msg[0]);
                 io.to(msg.Chat_Room_Id).emit('receiveMessage', db_msg[0]);
             }
         });
